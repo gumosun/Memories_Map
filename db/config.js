dd9ad203e2056ad466a02f2f7f92f3d9fca13b4b
Hao Sun <hao.sun@nyu.edu>
2017-09-18 15:44:06 -0400

    heroku version

--- db/config.js
+++ db/config.js
@@ -1,22 +1,40 @@
-const options = {
-  query: (e) => {
-    console.log(e.query);
-  }
-}
-const pgp = require('pg-promise')(options);
+// const options = {
+//   query: (e) => {
+//     console.log(e.query);
+//   }
+// }
+// const pgp = require('pg-promise')(options);
 
-function setDatabase() {
-  if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-    return pgp({
-      database: 'memories_dev',
-      port: 5432,
-      host: 'localhost',
-    })
-  } else {
-    return pgp(process.env.DATABASE_URL);
-  }
-}
+// function setDatabase() {
+//   if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+//     return pgp({
+//       database: 'memories_dev',
+//       port: 5432,
+//       host: 'localhost',
+//     })
+//   } else {
+//     return pgp(process.env.DATABASE_URL);
+//   }
+// }
 
-const db = setDatabase();
+// const db = setDatabase();
+
+// module.exports = db;
+
+
+// This is for Heroku
+const pgp = require('pg-promise')();
+
+let db;
+
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+  db = pgp({
+    database: 'adaquote_development',
+    port: 5432,
+    host: 'localhost',
+  });
+} else if (process.env.NODE_ENV === 'production') {
+  db = pgp(process.env.DATABASE_URL);
+}
 
 module.exports = db;
